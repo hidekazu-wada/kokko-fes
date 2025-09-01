8828c7cdc0acf71ce4d1c2d17481a48e0d119ebc
Hidekazu Wada <s.hazushi@gmail.com>
2025-09-02 07:36:25 +0900

    再生ページと視聴ページを削除し、グローバル型定義を追加し、TypeScript設定を更新する

---
+++ src/types/global.d.ts
@@ -0,0 +1,51 @@
+// Astroコンポーネントの型定義
+declare module "../components/common/MenuToggle.astro" {
+  const component: any;
+  export default component;
+}
+
+declare module "../components/common/Menu.astro" {
+  const component: any;
+  export default component;
+}
+
+declare module "../components/common/Footer.astro" {
+  const component: any;
+  export default component;
+}
+
+declare module "../components/common/PageHeader.astro" {
+  const component: any;
+  export default component;
+}
+
+declare module "../components/common/PageBottomWave.astro" {
+  const component: any;
+  export default component;
+}
+
+// カテゴリーデータの型定義
+declare module "../data/categories.json" {
+  interface CategoryItem {
+    name: string;
+    image: string;
+    location: string;
+    time: string;
+  }
+
+  interface CategoryData {
+    id: string;
+    title: string;
+    titleAccent: string;
+    subtitle: string;
+    description: string;
+    items: CategoryItem[];
+  }
+
+  interface CategoriesData {
+    [key: string]: CategoryData;
+  }
+
+  const data: CategoriesData;
+  export default data;
+}
